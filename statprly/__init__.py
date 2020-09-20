0991a4ea5be7e0abad9134013c84a444537ff2b4
Vlad Pavlichek <ember.toon@protonmail.com>
2020-09-20 23:01:46 +0300

    Create project structure (#1)
    Added project info and linters.
    
    Implemented:
    * `Travis CICD`
    * Linters
    * Makefile
    * Requirements levels
    * `Isort` added
    * Added setup package config file

---
+++ statprly/__init__.py
@@ -0,0 +1,7 @@
+"""
+Top-level module for `Statprly` pattern recognition package.
+This package interact with server and recognize a patterns.
+"""
+
+__version__ = "0.1.0"
+__version_info__ = tuple(int(i) for i in __version__.split(".") if i.isdigit())
