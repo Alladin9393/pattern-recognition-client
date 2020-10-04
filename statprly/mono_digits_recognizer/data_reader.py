dc69141260e818f36086b178006517ddec6e6202
Vlad Pavlichek <vlader112@gmail.com>
2020-10-04 22:50:39 +0300

    Implemented DataReader functionality
    Implemented `get_digit_standards_dict` and `__validate_data` in
    DataReader and added getters and seters for MonoDigitRecognizer.
    
    Implemented:
    * DataReader functionality
    * Integration DataReader with MonoDigitRecognizer
    * Seters and geters for MonoDigitRecognizer
    * Added tests for DataReader
    * Added custom errors and constants

--- statprly/mono_digits_recognizer/data_reader.py
+++ statprly/mono_digits_recognizer/data_reader.py
@@ -1,6 +1,12 @@
 """
 Provide implementation of the DataReader.
 """
+import json
+
+import numpy
+
+from statprly.errors import ValidationDataError
+from statprly.constants import SUPPORTED_DIMENSION_OF_STANDARDS
 
 
 class DataReader:
@@ -18,11 +24,34 @@ class DataReader:
         Read data from `digit_standards_path` and transform it into `dictionary` object
         where the key is the digit, value is the array of digit image data (array of `1` and `0`).
         """
+        with open(self.digit_standards_path) as f:
+            standards = json.loads(f.read())
+
+        self.__validate_data(standards)
+        return standards
 
     @staticmethod
-    def __validate_data(data: list):
+    def __validate_data(data: dict):
         """
         Digit standards data validation.
 
         :param data: digit data to be validated
         """
+        base_shape = numpy.array(data["0"]).shape
+        if len(base_shape) != SUPPORTED_DIMENSION_OF_STANDARDS:
+            raise ValidationDataError("Data shape validation error.")
+
+        base_digit_pixels_sum = base_shape[1]
+
+        for digit in data.values():
+            digit_data = numpy.array(digit)
+            digit_shape = digit_data.shape
+
+            if base_shape != digit_shape:
+                raise ValidationDataError("Data shape validation error.")
+
+            for pixels in digit:
+                sum_of_pixels = sum(pixels)
+
+                if sum_of_pixels > base_digit_pixels_sum:
+                    raise ValidationDataError("Invalid pixel in data.")
